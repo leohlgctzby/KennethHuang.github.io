9d575b89eb154eb15526fb9a2946fa61bdb2a5fd
Kenneth-Huang <kenneth.p.huang@gmail.com>
2018-09-04 09:25:05 +1000

    4/Sep/2018-Partial responsive

--- ITJiangren/Resume/js/main.js
+++ ITJiangren/Resume/js/main.js
@@ -1,6 +1,37 @@
-(function($){
+(function ($) {
+
+    console.log("main.js loaded");
+    // Hide Mobile menu
+    function mobileMenuHide() {
+        console.log("OK in mobileMenuHide()");
+        var windowWidth = $(window).width();
+        if (windowWidth < 1024) {
+            $('#site_header').addClass('mobile-menu-hide');
+        }
+    }
+    // /Hide Mobile menu
+
+    //On document load
+    // $(document).on("ready", function() {
+    $(document).ready(function(){
+        console.log("OK in document onReady()");
+        // Mobile menu
+        $('.menu-toggle').on("click", function () {
+            console.log("OK, div.menu-toggle clicked");
+            $('#site_header').toggleClass('mobile-menu-hide');
+        });
+
+        // Mobile menu hide on main menu item click
+        $('.main-menu').on("click", "a", function (e) {
+            console.log("OK, div.main-menu clicked");
+            mobileMenuHide();
+        });
 
 
+    });
 
-    
+    console.log("main.js ended");
+
 })(jQuery);
+
+
